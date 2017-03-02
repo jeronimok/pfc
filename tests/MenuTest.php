f2df61909efcc0b58d65200eb6b4f7f5e81ee279
Jeronimo <jeronimo.calace@gmail.com>
2017-03-02 20:32:36 -0300

    correccion de identacion

--- tests/MenuTest.php
+++ tests/MenuTest.php
@@ -25,42 +25,42 @@ class MenuTest extends TestCase
         	'role' => 'general',
         	'email' => 'general@gmail.com',
         	'password' =>  bcrypt('admin')
-        ]);
-        $this->actingAs($user)
-        	->visit('/')
-        	->dontSee('Administrar plataforma');
-        $this->actingAs($user)
-        	->get('/administracion')
-        	->seeStatusCode('404');
+                ]);
+                $this->actingAs($user)
+                	->visit('/')
+                	->dontSee('Administrar plataforma');
+                $this->actingAs($user)
+                	->get('/administracion')
+                	->seeStatusCode('404');
 
-        // Crear usuario action_admin
-		$user = factory(App\User::class)->create([
-        	'name' => 'Jerónimo',
-        	'role' => 'action_admin',
-        	'email' => 'actionadmin@gsmail.com',
-        	'password' =>  bcrypt('admin')
-        ]);
-        $this->actingAs($user)
-        	->visit('/')
-        	->dontSee('Administrar plataforma');
-        $this->actingAs($user)
-        	->get('/administracion')
-        	->seeStatusCode('404');
-        	
+                // Crear usuario action_admin
+        		$user = factory(App\User::class)->create([
+                	'name' => 'Jerónimo',
+                	'role' => 'action_admin',
+                	'email' => 'actionadmin@gsmail.com',
+                	'password' =>  bcrypt('admin')
+                ]);
+                $this->actingAs($user)
+                	->visit('/')
+                	->dontSee('Administrar plataforma');
+                $this->actingAs($user)
+                	->get('/administracion')
+                	->seeStatusCode('404');
+                	
 
-        // Crear usuario admin
-		$user = factory(App\User::class)->create([
-        	'name' => 'Jerónimo',
-        	'role' => 'admin',
-        	'email' => 'jeronimo.calace@gmail.com',
-        	'password' =>  bcrypt('admin')
-        ]);
-        $this->actingAs($user)
-        	->visit('/')
-        	->see('Administrar plataforma');
+                // Crear usuario admin
+        		$user = factory(App\User::class)->create([
+                	'name' => 'Jerónimo',
+                	'role' => 'admin',
+                	'email' => 'jeronimo.calace@gmail.com',
+                	'password' =>  bcrypt('admin')
+                ]);
+                $this->actingAs($user)
+                	->visit('/')
+                	->see('Administrar plataforma');
 
-        $this->click('Administrar plataforma')
-        	->seePageIs('administracion')
-        	->see('Administración');
-	}
+                $this->click('Administrar plataforma')
+                	->seePageIs('administracion')
+                	->see('Administración');
+        	}
 }
