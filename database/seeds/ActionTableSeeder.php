0db797e8b0624484ec879f8edaee84c9fc11e16d
Jeronimo <jeronimo.calace@gmail.com>
2017-03-09 17:21:47 -0300

    modificado seeder de acciones

--- database/seeds/ActionTableSeeder.php
+++ database/seeds/ActionTableSeeder.php
@@ -14,15 +14,19 @@ class ActionTableSeeder extends Seeder
         DB::table('actions')->truncate();
 
         factory(App\Action::class)->create([
-        	'title' => 'Consejo de niños y niñas',
-        	'description' => 'El consejo de niños y nilas es...',
-        	'admin_email' => 'jeronimo.calace@gmail.com',
-        	'admin_id'		=> 1,
-        	'create_p'		=> 1,
-        	'debate_p'		=> 1,
-        	'support_p'		=> 1,
-        	'opt_p'		=> 1,
-        	'audit'		=> 1
+        	'title'        => 'Consejo de niños y niñas',
+
+        	'description'  => 'Constituye un dispositivo educativo para el ejercicio temprano de la ciudadanía que genera espacios en los cuales, mediante el diálogo, la interacción y el trabajo en equipo se buscan establecer acuerdos, entre los adultos y los niños, que sustenten la idea de ciudad inclusiva, generando propuestas concretas y promoviendo acciones y actitudes en pos de transformar el espacio urbano. La idea del proyecto es hacer efectiva la participación de los niños y niñas en las cuestiones que conciernen a la ciudad, atendiendo a sus necesidades y expectativas. Niños y niñas, entre 8 y 11 años, de los ocho distritos de la ciudad ejercen su derecho a opinar y a ser oídos mediante el trabajo en las escuelas y en la sesión anual en la que se encuentran con el Intendente de la ciudad para presentarle sus proyectos.',
+
+        	'admin_email'  => 'jeronimo.calace@gmail.com',
+        	'admin_id'     => 1,
+        	'create_p'     => 1,
+        	'debate_p'     => 1,
+        	'support_p'    => 1,
+        	'opt_p'        => 1,
+        	'audit'        => 1
         ]);
+
+        factory(App\Action::class, 11)->create();
     }
 }
