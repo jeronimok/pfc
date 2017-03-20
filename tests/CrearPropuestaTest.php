8b651606dd31d11735c293c22ba75a44fbd68623
Jeronimo <jeronimo.calace@gmail.com>
2017-03-20 11:11:33 -0300

    creacion de test

---
+++ tests/CrearPropuestaTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CrearPropuestaTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testCrearPropuesta()
+    {
+        $general = $this->createUSer('general');
+
+        $this->actingAs($general)
+        	->visit('accion-participativa/1')
+        	->see('Crear propuesta')
+        	->click('Crear propuesta')
+        	->seePageIs('accion-participativa/crear-propuesta')
+        	->see('NUEVA PROPUESTA')
+        	->type('Nuevos juegos en las plazas', 'title')
+        	->type('Necesitamos nuevos juegos en las plazas porque... Y propongo hacer...', 'content')
+        	->press('Publicar propuesta')
+        	->seePageIs('accion-participativa/1')
+        	->see('La propuesta ha sudo creada con éxito')
+        	->seeInDataBase('proposals', [
+        		'title' => 'Nuevos juegos en las plazas',
+        		'content' => 'Necesitamos nuevos juegos en las plazas porque... Y propongo hacer...'
+        		]);
+
+
+
+    }
+}
