2f2cdb622937094c0051f961471721d90913cf3a
Jeronimo <jeronimo.calace@gmail.com>
2017-04-13 15:18:24 -0300

    update home

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -4,8 +4,8 @@
 <div class="jumbotron">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-          <h1>Participación ciudadana</h1>
+      <div class="col-md-10 col-md-offset-1" align="center">
+          <h1 >PARTICIPACIÓN CIUDADANA</h1>
           <p>Bienvenidos a la plataforma web de participación ciudadana de la Ciudad de Santa Fe</p>
       </div>
     </div>
