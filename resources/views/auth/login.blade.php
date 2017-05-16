cacee0767a318e08193a42e9b5fd74f133cb10cf
Jeronimo <jeronimo.calace@gmail.com>
2017-05-16 10:26:32 -0300

    Rediseño de forms de registro y login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,57 +1,53 @@
 @extends('layout')
 
 @section('content')
-<div class="jumbotron jumbo-full">
+
+<div class="jumbotron">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">@lang('auth.login')</div>
-					<div class="panel-body">
-						@include('partials/errors')
+			<div class="col-md-4 col-md-offset-4">
+				<h2>Iniciar sesión</h2>
+			</div>
+		</div>
+	</div>
+</div>
 
-						@include('partials/success')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			@include('partials/errors')
+			@include('partials/success')
 
-						<form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-							<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<form role="form" method="POST" action="{{ route('login') }}">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-							<div class="form-group">
-								<label class="col-md-4 control-label">@lang('validation.attributes.email')</label>
-								<div class="col-md-6">
-								    <input type="email" name="email" value="{{ old('email') }}" class="form-control">
-								</div>
-							</div>
+				<div class="form-group">
+					<label class="control-label">@lang('validation.attributes.email')</label>
+					<input type="email" name="email" value="{{ old('email') }}" class="form-control">
+				</div>
 
-							<div class="form-group">
-								<label class="col-md-4 control-label">@lang('validation.attributes.password')</label>
-								<div class="col-md-6">
-									<input type="password" name="password" class="form-control">
-								</div>
-							</div>
+				<div class="form-group">
+					<label class="control-label">@lang('validation.attributes.password')</label>
+					<input type="password" name="password" class="form-control">
+				</div>
 
-							<div class="form-group">
-								<div class="col-md-6 col-md-offset-4">
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="remember">@lang('auth.remember_me')
-										</label>
-									</div>
-								</div>
-							</div>
-
-							<div class="form-group">
-								<div class="col-md-6 col-md-offset-4">
-									<button type="submit" class="btn btn-default" style="margin-right: 15px;">
-										@lang('auth.login_button')
-									</button>
-
-									<a href="/password/email">@lang('auth.forgot_password')</a>
-								</div>
-							</div>
-						</form>
+				<div class="form-group">
+					<div class="checkbox">
+						<label>
+							<input type="checkbox" name="remember">@lang('auth.remember_me')
+						</label>
 					</div>
 				</div>
-			</div>
+
+				<div class="form-group">
+						<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+							<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>  @lang('auth.login_button')
+						</button>
+
+						<a href="/password/email">@lang('auth.forgot_password')</a>
+					</div>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,57 +1,52 @@
 @extends('layout')
 
 @section('content')
-<div class="jumbotron jumbo-full">
+
+<div class="jumbotron">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">@lang('auth.register')</div>
-					<div class="panel-body">
-						@include('partials/errors')
-
-						<form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-							<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-							<div class="form-group">
-								<label class="col-md-4 control-label">@lang('validation.attributes.name')</label>
-								<div class="col-md-6">
-									<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-								</div>
-							</div>
-
-							<div class="form-group">
-								<label class="col-md-4 control-label">@lang('validation.attributes.email')</label>
-								<div class="col-md-6">
-									<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-								</div>
-							</div>
-
-							<div class="form-group">
-								<label class="col-md-4 control-label">@lang('validation.attributes.password')</label>
-								<div class="col-md-6">
-									<input type="password" class="form-control" name="password">
-								</div>
-							</div>
-
-							<div class="form-group">
-								<label class="col-md-4 control-label">@lang('validation.attributes.password_confirmation')</label>
-								<div class="col-md-6">
-									<input type="password" class="form-control" name="password_confirmation">
-								</div>
-							</div>
-
-							<div class="form-group">
-								<div class="col-md-6 col-md-offset-4">
-									<button type="submit" class="btn btn-primary">
-										@lang('auth.register_button')
-									</button>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
+			<div class="col-md-4 col-md-offset-4">
+				<h2>Registrarse</h2>
 			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			@include('partials/errors')
+			@include('partials/success')
+
+			<form role="form" method="POST" action="{{ route('register') }}">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<div class="form-group">
+					<label class="control-label">@lang('validation.attributes.name')</label>
+					<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+				</div>
+
+				<div class="form-group">
+					<label class="control-label">@lang('validation.attributes.email')</label>
+					<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+				</div>
+
+				<div class="form-group">
+					<label class="control-label">@lang('validation.attributes.password')</label>
+					<input type="password" class="form-control" name="password">
+				</div>
+
+				<div class="form-group">
+					<label class="control-label">@lang('validation.attributes.password_confirmation')</label>
+					<input type="password" class="form-control" name="password_confirmation">
+				</div>
+
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary">
+						<span class="glyphicon glyphicon-user" aria-hidden="true"></span> @lang('auth.register_button')
+					</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
